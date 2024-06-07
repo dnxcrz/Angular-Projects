38bd03e256ad4cf7d1ba866dec5dd6466f903655
Dionix Cruz <dionixcruz@gmail.com>
2024-06-07 13:57:59 -0400

    -Added dynamic list output for users -Added Task section(to be develop) -Added onclickEvent for users tasks -Code improvement

--- mando-task/src/app/user/user.component.ts
+++ mando-task/src/app/user/user.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+type User = {
+  id: string;
+  avatar: string;
+  name: string;
+};
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
-
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
   }
 }
