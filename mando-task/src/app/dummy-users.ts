ca9c3943f6b407c4e34aaa791c8995870cbc7ffc
Dionix Cruz <dionixcruz@gmail.com>
2024-06-07 11:19:48 -0400

    Angular projects - first commit
    working on my first angular project - Mando Task

---
+++ mando-task/src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+    {
+      id: 'u1',
+      name: 'Din Djarin',
+      avatar: 'user-1.jpg',
+    },
+    {
+      id: 'u2',
+      name: 'Din Grogu',
+      avatar: 'user-2.jpg',
+    },
+    {
+      id: 'u3',
+      name: 'Bo-Katan Kryze',
+      avatar: 'user-3.jpg',
+    },
+    {
+      id: 'u4',
+      name: 'Paz Vizsla',
+      avatar: 'user-4.jpg',
+    },
+    {
+      id: 'u5',
+      name: 'Boba Fett',
+      avatar: 'user-5.jpg',
+    },
+    {
+      id: 'u6',
+      name: 'The Armorer',
+      avatar: 'user-6.jpg',
+    },
+  ];
