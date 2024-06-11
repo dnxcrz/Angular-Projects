32f4426a782111c00b813ca87913a8954303d847
Dionix Cruz <dionixcruz@gmail.com>
2024-06-11 11:27:06 -0400

    -Added browser storage to save task, this might be updated to use a database  in the future.

--- mando-task/src/app/tasks/tasks.service.ts
+++ mando-task/src/app/tasks/tasks.service.ts
@@ -57,6 +57,14 @@ export class TaskService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -69,9 +77,15 @@ export class TaskService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
