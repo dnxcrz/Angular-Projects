1c2db0c6a9ddbd0017045c51b5195f6e5a05bd5f
Dionix Cruz <dionixcruz@gmail.com>
2024-07-03 09:15:17 -0400

    -Optimized CSS Files -Added Shared/Control files

--- third-project/src/app/dashboard/tickets/tickets.component.ts
+++ third-project/src/app/dashboard/tickets/tickets.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { NewTicketComponent } from '../../tickets/new-ticket/new-ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [ButtonComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
+  imports: [ButtonComponent, NewTicketComponent],
 })
 export class TicketsComponent {}
